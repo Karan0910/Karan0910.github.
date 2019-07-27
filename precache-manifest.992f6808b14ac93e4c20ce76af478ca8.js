6e4e4a1e54749370c05914e5407b65238d4e0bd4
Karan0910 <95sharma.karan@gmail.com>
2019-07-27 01:17:24 -0400

    Resume initial

---
+++ precache-manifest.992f6808b14ac93e4c20ce76af478ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c660e272844683073b25217dbabff6a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "581bbe8152447bb885aa",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "581bbe8152447bb885aa",
+    "url": "/static/js/main.f8e9d618.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
